378900511235a773f3756c1759b2e2c64487bf98
Aswin Lakshman <shwin.lakes@gmail.com>
2018-04-19 22:39:38 +1000

    Added keyboard test

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { runGalaxy } from './team-cotton-galaxy';
 import { runInputTest } from './input-test';
 
 console.log('Running galaxy demo');
-runGalaxy();
+runInputTest();
--- input-test/index.js
+++ input-test/index.js
@@ -1,3 +1,52 @@
+import Cloud from "../team-cotton-galaxy/cloud";
+import { Animator, Compositor, Layer, Entity, util, input } from "cotton-js";
+
+class SimpleEntity extends Entity {
+    constructor(pos, vel, size) {
+        super(pos, vel, size, [], true);
+    }
+
+    // do nothing. We just want to see a box
+    draw() { }
+};
+
 export const runInputTest = function run() {
-    console.log('Implement me');
+    const rootEl = document.getElementById('yaboi');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    
+    const movableEntity = new SimpleEntity(
+        new util.Point(0, 0),
+        new util.Point(0, 0),
+        new util.Point(50, 50)
+    );
+
+    const inputHandler = new input.Keyboard(window);
+    
+    inputHandler.addMapping('ArrowLeft', () => {
+        movableEntity.pos.x -= 10;
+    });
+
+    inputHandler.addMapping('ArrowRight', () => {
+        movableEntity.pos.x += 10;
+    });
+
+    inputHandler.addMapping('ArrowUp', () => {
+        movableEntity.pos.y -= 10;
+    });
+
+    inputHandler.addMapping('ArrowDown', () => {
+        movableEntity.pos.y += 10;
+    });
+
+    let animator = new Animator(
+        new Compositor(
+            width,
+            height,
+            rootEl,
+            [new Layer(width, height, [movableEntity])],
+        )
+    );
+
+    animator.start();
 };
