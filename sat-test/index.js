c9a2b63dcd51ffd97094daf94887669fdef31012
Aswin Lakshman <shwin.lakes@gmail.com>
2018-07-21 14:05:00 +1000

    Test for sat test

--- sat-test/index.js
+++ sat-test/index.js
@@ -5,36 +5,45 @@ import {
   PolygonEntity,
   EntityLibrary,
   util,
+  Trait,
   traits,
   input,
+  detectCollisionsSAT,
 } from 'cotton-js';
-import SimpleEntity from '../common/simple-entity';
 
 const { Mouse, MOVE } = input;
 const { Obstacle } = traits;
 const { Vector2, Polygon } = util;
 
+class Collidable extends Trait {
+  constructor(entity) {
+    super(entity);
+  }
+
+  update(deltaTime) {
+    this.resolveCollisions();
+  }
+
+  resolveCollisions() {
+    const collisionStrategy = detectCollisionsSAT;
+    const collisions = collisionStrategy(this.entity, ['Obstacle']);
+
+    if (!collisions || !collisions.length) return;
+
+    collisions.forEach((collision) => {
+      this.entity.position.x -= collision.response.overlapV.x;
+      this.entity.position.y -= collision.response.overlapV.y;
+    });
+  }
+}
+
 export default class SimplePolygonEntity extends PolygonEntity {
-  constructor(pos, entityLibrary, traits) {
-    super(pos, new Polygon(
-      [new Vector2(100, 50), new Vector2(50, 100), new Vector2(0, 90)]
-    ), entityLibrary, traits, true);
+  constructor(pos, entityLibrary, shape) {
+    super(pos, shape, entityLibrary, [], true);
   }
 
   draw() {
     return;
-  }
-}
-
-class Block extends SimpleEntity {
-  constructor(pos, entityLibrary) {
-    super(
-      pos,
-      new Vector2(50, 50),
-      entityLibrary,
-      'black',
-      [new Obstacle()]
-    );
   }
 }
 
@@ -48,7 +57,20 @@ export const runSatTest = function runSatTest() {
 
   const entities = [];
 
-  const daBoi = new SimplePolygonEntity(new Vector2(50, 50), entityLibrary, []);
+  const daBoi = new SimplePolygonEntity(new Vector2(50, 50), entityLibrary, new Polygon([
+    new Vector2(0, 0),
+    new Vector2(30, 0),
+    new Vector2(0, 30),
+  ]));
+  daBoi.addTraits([new Collidable(daBoi)]);
+
+  const obstacle = new SimplePolygonEntity(new Vector2(370, 370), entityLibrary, new Polygon([
+    new Vector2(15, 5),
+    new Vector2(100, 50),
+    new Vector2(50, 100),
+    new Vector2(10, 90),
+  ]));
+  obstacle.addTraits([new Obstacle()]);
 
   const mouse = new Mouse(document.documentElement);
   mouse.addMapping(MOVE, ({ pointerPosition }) => {
@@ -57,7 +79,7 @@ export const runSatTest = function runSatTest() {
 
   entities.push(
     daBoi,
-    new Block(new Vector2(370, 370), entityLibrary),
+    obstacle,
   );
 
   let animator = new Animator(
